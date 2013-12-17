3c33efe6cf6edfc5387dd2a6f0d1ec2c2da4f1a5
Lingjun Jiang <lingjun.jiang@itbconsult.com>
2013-12-17 17:00:12 +0800

    Added soap module.

--- web.js
+++ web.js
@@ -5,10 +5,23 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World! (' + (process.env.COMPANY_NAME || "ITBconsult") + " - " + (process.env.NODE_EVN || "development") + ")");
+  res.send('Welcome to Salesforce Migration Tool! (' + (process.env.COMPANY_NAME || "ITBconsult") + " - " + (process.env.NODE_EVN || "development") + ")");
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+
+app.configure('production', function() {
+  // Do production-specific action
+  console.log("running on production");
+});
+app.configure('staging', function() {
+  // Do staging-specific action
+  console.log("running on staging");
+});
+app.configure('development', function() {
+  // Do some development-specific action
+  console.log("running on development");
 });
