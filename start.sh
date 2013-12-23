abe98b2d0eca4b163cb914ff33b10e9957b39979
Lingjun Jiang <lingjun.jiang@itbconsult.com>
2013-12-23 21:48:19 +0800

    Ignored file

--- start.sh
+++
@@ -1,8 +0,0 @@
-clear
-export PORT=5000
-export COMPANY_NAME=ITBconsult
-export NODE_ENV=development
-export TEMP_USERNAME=lingjun.jiang@itbconsult.com.test
-export TEMP_PASSWORD=sfdc123456
-export TEMP_SECURETOKEN=8Y0hd3FexVQjU3NOqPsT87Lpc
-foreman start
